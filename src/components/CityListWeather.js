a92284a26e8501f915598651ddec29ca34439393
Roberto Ribeiro <robertov82008@gmail.com>
2017-03-16 17:23:18 -0300

    fix component citylistweather media

--- src/components/CityListWeather.js
+++ src/components/CityListWeather.js
@@ -2,8 +2,44 @@ import React from 'react'
 import CityListWeatherDescription from './CityListWeatherDescription.js'
 
 class CityListWeather extends React.Component {
+  /*
+  constructor (props) {
+    super(props)
+    const {listWeathers} = props
+
+    console.log(Object.keys(listWeathers).reduce((previous, key) => {
+      let list = 0
+      list += listWeathers[key].main.temp
+
+      return list
+    }))
+
+    const cityListWeatherTemp = props.listWeathers.reduce((previous, current) => {
+      return previous.main.temp + current.main.temp
+    })
+
+    this.state = {
+      temp: 0
+    }
+
+  }
+  */
+
   render () {
     const {listWeathers} = this.props
+
+    const cityListWeatherTemp = listWeathers.map((listWeather) => {
+      return listWeather.main.temp
+    }).reduce((previous, current) => (previous + current))
+
+    const cityListWeatherPressure = listWeathers.map((listWeather) => {
+      return listWeather.main.pressure
+    }).reduce((previous, current) => (previous + current))
+
+    const cityListWeatherHumidity = listWeathers.map((listWeather) => {
+      return listWeather.main.humidity
+    }).reduce((previous, current) => (previous + current))
+
     const cityListWeather = listWeathers.map((listWeather, key) => {
       return (
         <div key={key} className="thumbnail">
@@ -22,6 +58,15 @@ class CityListWeather extends React.Component {
     return (
       <div>
         <h2> Weathers</h2>
+
+        <h3>Media</h3>
+        <ul>
+          <li>Temperatura: {cityListWeatherTemp / listWeathers.length}°C</li>
+          <li>Pressão: {cityListWeatherPressure / listWeathers.length}hPa</li>
+          <li>Humidade: {cityListWeatherHumidity / listWeathers.length}%</li>
+        </ul>
+
+        <h3>List</h3>
         {cityListWeather}
       </div>
     )
